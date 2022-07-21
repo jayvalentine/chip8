4ed22190b18112caea23ca2a66326854a17244a7
Jay Valentine <jayv136@gmail.com>
2022-07-21 20:04:51 +0100

    Implemented decoding of register/index set/add imm

--- src/decode.c
+++ src/decode.c
@@ -1,5 +1,12 @@
 #include "include/decode.h"
 
+#define EXTRACT_XNN(i, o)     \
+    i->X = (o >> 8) & 0x000f; \
+    i->NN = o & 0x00ff
+
+#define EXTRACT_NNN(i, o)     \
+    i->NNN = o & 0x0fff
+
 void decode(Instruction * instr, uint16_t opcode)
 {
     uint8_t opcode_nibble = opcode >> 12;
@@ -11,7 +18,19 @@ void decode(Instruction * instr, uint16_t opcode)
             break;
         case 0x1:
             instr->opcode = JUMP;
-            instr->NNN = opcode & 0x0fff;
+            EXTRACT_NNN(instr, opcode);
+            break;
+        case 0x6:
+            instr->opcode = SET_REG_IMM;
+            EXTRACT_XNN(instr, opcode);
+            break;
+        case 0x7:
+            instr->opcode = ADD_REG_IMM;
+            EXTRACT_XNN(instr, opcode);
+            break;
+        case 0xA:
+            instr->opcode = SET_INDEX_IMM;
+            EXTRACT_NNN(instr, opcode);
             break;
     }
 }
